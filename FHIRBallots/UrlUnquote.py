03c0b8f66eb62a6611220fba9c11417972032f4a
jrhoads <john_rhoads@acm.org>
2018-02-23 17:44:37 -0500

    change option processing - next, use inputfile, add more help information via optparse, simplify

--- FHIRBallots/UrlUnquote.py
+++ FHIRBallots/UrlUnquote.py
@@ -3,8 +3,10 @@ Unquote a string with url encoding, for use on output of ballot reconciliation X
 
 """
 
+import sys
 import urllib.parse
 import optparse
+import os.path
 
 
 def unquote(url_str):
@@ -12,9 +14,29 @@ def unquote(url_str):
 
 
 if __name__ == "__main__":
-    filename = "../PoCDSubsetList.html"
-    f = open(filename)
+    p = optparse.OptionParser()
+    p.add_option("-o", action="store", dest="outfile")
+    p.add_option("--output", action="store", dest="outfile")
+    # p.add_option("-x", action="store_true", dest = "xhtml")
+    # p.add_option("--html", action="store_true", dest = "xhtml")
+    # p.add_option("--wiki", action="store_true", dest = "wiki")
+    # p.set_defaults(xhtml=True, wiki=True)
+    opts, args = p.parse_args()
+    fout = None
+    if opts.outfile:
+        fout = open(opts.outfile, "w")
+    else:
+        fout = sys.stdout
+
+    if len(args):
+        infilename = args[0]
+    else:
+        infilename = "BallotSubsetListRaw.html"
+
+    if not os.path.exists(infilename):
+        print("input file ", infilename, "not found")
+    f = open(infilename)
     s = f.read()
     f.close()
     s = unquote(s)
-    print(s)
+    print(s, file=fout)
